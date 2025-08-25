c86a21dfa1992ee35ac68914471f0006a1fdda1c
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-08-25 14:44:29 +0200

    v1

--- src/app/ui/GDPRCookies.jsx
+++ src/app/ui/GDPRCookies.jsx
@@ -65,7 +65,6 @@ export default function GDPRCookies() {
             }
         });
 
-        console.log("Google Analytics removed");
     }, []);
 
     const handleAcceptAll = useCallback(() => {
